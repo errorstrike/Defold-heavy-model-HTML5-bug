ecfb62703fe7d22c160d66159d76703c69482c41
errorstrike <successful7person@gmail.com>
2024-07-14 11:39:42 +0300

    first commit

---
+++ assets/general_ship_repair/general_ship_repair.go
@@ -0,0 +1,29 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/assets/general_ship_repair/general_ship_repair_03.glb\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"{{NAME}}\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/builtins/materials/model.material\"\n"
+  "  textures {\n"
+  "    sampler: \"tex0\"\n"
+  "    texture: \"/assets/general_ship_repair/General_Ship_Repair_01.jpg\"\n"
+  "  }\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
